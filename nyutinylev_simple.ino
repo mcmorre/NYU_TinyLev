8a42c39a2a63790d8bbdc6de074c24cf3d9023e7
mcmorre <52986587+mcmorre@users.noreply.github.com>
2022-05-27 11:27:39 -0400

    Add files via upload
    added comments

--- nyutinylev_simple.ino
+++ nyutinylev_simple.ino
@@ -5,7 +5,7 @@ to run the TinyLev (Marzo 2017 https://doi.org/10.1063/1.4989995).
 
 @author: David Grier
 @modified: Mia Morrell
-Modified March 29, 2022
+Modified May 27, 2022
 
 
 The TinyLev uses an L298N dual H-bridge driver to power the
@@ -14,15 +14,20 @@ upper and lower arrays of piezoelectric transducers.
 The nominal resonant frequency of these transducers is 40 kHz.
 
 Each H-bridge is controlled by two pins which should alternate 
-between 10 and 01 at the driving frequency. Each of the FlexPWM modules
-in the Teensy 4.0 can be programmed to provide this pattern 
-by setting the "invert" bit on channel B.
+between 10 and 01 at the driving frequency. The FlexPWM modules output signal
+on 2 channels, channel A and channel B. Each of the FlexPWM modules in the Teensy 4.0 
+can be programmed to provide this alternating pattern by 
+setting the "invert" bit on channel B.
+
 Suitable pairs of pins are:
    pins 2 and 3    FlexPWM4_2A and FlexPWM4_2B
    pins 6 and 9    FlexPWM2_2A and FlexPWM2_2B
    pins 7 and 8    FlexPWM1_3B and FlexPWM1_3A
    pins 22 and 23  FlexPWM1_3B and FlexPWM1_3A 
-see https://forum.pjrc.com/threads/62880-Teensy-4-0-PWM-reversing-H-Bridge-driver*/
+see https://forum.pjrc.com/threads/62880-Teensy-4-0-PWM-reversing-H-Bridge-driver 
+
+For documentation on FlexPWM, see https://www.pjrc.com/teensy/IMXRT1060RM_rev3.pdf 
+beginning pg 3091. */
 
  
 
@@ -32,20 +37,22 @@ see https://forum.pjrc.com/threads/62880-Teensy-4-0-PWM-reversing-H-Bridge-drive
 #define PWMSTEPS 256    // to match PWMRES: there are 256 steps
 
 #define INVERTB 0x0200; // bit pattern to invert channel B: make the pins alternate between 10 and 01 
+                        // this inversion of signals is required to drive the H-bridge: https://en.wikipedia.org/wiki/H-bridge
 
+// choose the pins that we will use to send the PWM signals
 const int upperA = 2;   // FlexPWM4_2A
 const int upperB = 3;   // FlexPWM4_2B
 const int lowerA = 6;   // FlexPWM2_2A
 const int lowerB = 9;   // FlexPWM2_2B
 
-// blink the LED to show that the driver is running:
+// initialize LED blinking to show that the driver is running:
 const int ledPin = LED_BUILTIN;
 const long blinkDuration = 500;
 unsigned long lastBlink = 0;
 int ledState = LOW;
 
 // here, set the same amplitude for all the pins
-void setAmplitude(uint16_t amp) {
+void setAmp(uint16_t amp) {
   Serial.printf("\namplitude: %u ", amp);
   analogWrite(upperA, amp); 
   analogWrite(upperB, amp);
@@ -58,7 +65,7 @@ void setAmplitude(uint16_t amp) {
 
 // set the pin frequencies: here frq is the driving frequency
 // set the same frequency for all pins
-void setFrequency(float frq) {
+void setFreq(float frq) {
   analogWriteFrequency(upperA, frq);
   analogWriteFrequency(upperB, frq);
   analogWriteFrequency(lowerA, frq);
@@ -66,7 +73,7 @@ void setFrequency(float frq) {
 }
 
 // show the values of the output control register
-void ShowFlexPWMInit(void) {
+void showFlexPWMInit(void) {
   Serial.println("FlexPWM2:" );
   Serial.printf("  SM0OCTL: %04X ", FLEXPWM2_SM0OCTRL); 
   Serial.printf("  SM1OCTL: %04X ", FLEXPWM2_SM1OCTRL);
@@ -82,30 +89,30 @@ void ShowFlexPWMInit(void) {
 
 // fun blinking for testing
 void blink(void) {
-  unsigned long time = millis();
+  unsigned long getTime = millis();
 
-  if (time - lastBlink >= blinkDuration) {
-    lastBlink = time;
-    ledState = !ledState;
+  if (getTime - lastBlink >= blinkDuration) { 
+    lastBlink = getTime;
+    ledState = !ledState; // turn on/off LED
     digitalWrite(ledPin, ledState);
   }
 }
 
 // set the frequency and amplitude 
-void setup() {
+void setup(void) {
   pinMode(ledPin, OUTPUT);
   
   Serial.begin(9600);
   delay(100); // wait for PC to respond
 
   Serial.print("\nNYU TinyLev Driver");
-  analogWriteRes(PWMRES);
+  analogWriteRes(PWMRES); // write PWM resolution
   Serial.print("\nbefore initialization:");
-  ShowFlexPWMInit();
-  setFrequency(FREQUENCY);
-  setAmplitude(PWMSTEPS/2 - 1);
+  s     howFlexPWMInit();
+  setFreq(FREQUENCY);      // set the signal frequency
+  setAmp(PWMSTEPS/2 - 1); // set PWM amplitude to 50% duty cycle
   Serial.print("\nafter initialization:");
-  ShowFlexPWMInit();
+  showFlexPWMInit();
 }
 
 void loop() {
